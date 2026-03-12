6206960efdfd1738f068a6083b95cb1610049fb1
raddad772 <81476102+raddad772@users.noreply.github.com>
2026-03-12 10:14:29 -0400

    updated fixed tests

--- transcode_json.py
+++ transcode_json.py
@@ -11,22 +11,21 @@ def load_state(buf, ptr) -> (int, Any):
     start_ptr = ptr
     state = {'R' : []}
     for i in range (0, 32):
-        if i == 0: print('OFFSET ' + str(ptr))
-        state['R'].append(unpack_from('I', buf, ptr)[0])
+        state['R'].append(unpack_from('=I', buf, ptr)[0])
         ptr += 4
-    state['hi'], state['lo'], state['EPC'], state['PC'] = unpack_from('IIII', buf, ptr)
+    state['hi'], state['lo'], state['EPC'], state['PC'] = unpack_from('=IIII', buf, ptr)
     if state['R'][0] != 0:
         print('WARN R0 != 0!')
     ptr += 16
 
     #load target, slot, take
-    ltarget, lslot, ltake = unpack_from('III', buf, ptr)
+    ltarget, lslot, ltake = unpack_from('=III', buf, ptr)
     ptr += 12
     lslot = True if lslot != 0 else False
     ltake = True if ltake != 0 else False
 
     #branch i32 target, u32 val
-    btarget, bval = unpack_from('iI', buf, ptr)
+    btarget, bval = unpack_from('=iI', buf, ptr)
     ptr += 8
     state['delay'] = {'load': {'slot': lslot, 'take': ltake, 'target': ltarget},
                       'branch': {'target': btarget, 'val': bval} }
@@ -36,7 +35,7 @@ def load_state(buf, ptr) -> (int, Any):
 
 def load_cycles(buf, ptr) -> (int, Dict):
     start_ptr = ptr
-    num_cycles = unpack_from('I', buf, ptr)[0]
+    num_cycles = unpack_from('=I', buf, ptr)[0]
     ptr += 4
     cycles = []
 
@@ -50,20 +49,20 @@ def load_cycles(buf, ptr) -> (int, Dict):
     return full_sz, cycles
 
 def load_opcodes(buf, ptr):
-    full_sz = unpack_from('i', buf, ptr)[0]
+    full_sz = unpack_from('=i', buf, ptr)[0]
     opcodes = []
     ptr += 8
-    values = unpack_from('IIIII', buf, ptr)
+    values = unpack_from('=IIIII', buf, ptr)
     return full_sz, list(values)
 
 def decode_test(buf, ptr) -> (int, Dict):
     test = {}
     start_ptr = ptr
 
-    name = unpack_from('51p', buf, ptr)[0]
+    name = unpack_from('=51p', buf, ptr)[0]
     test['name'] = name.decode('ascii')
     ptr += 51
-    test['opcode'], test['opcode_addr'] = unpack_from('II', buf, ptr)
+    test['opcode'], test['opcode_addr'] = unpack_from('=II', buf, ptr)
     ptr += 8
     sz, test['initial'] = load_state(buf, ptr)
     ptr += sz
@@ -83,7 +82,7 @@ def decode_file(infilename, outfilename):
         content = infile.read()
     ptr = 0
     tests = []
-    NUMTESTS = unpack_from('i', content, ptr)[0]
+    NUMTESTS = unpack_from('=i', content, ptr)[0]
     ptr += 4
     for i in range(0, NUMTESTS):
         sz, test = decode_test(content, ptr)
